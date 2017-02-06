7d346ca17f4ebf2d475e1cdca1f3fe4d1350a4d3
Sebastian Olewczynski <sebastian.olewczynski@betbright.com>
2017-02-06 15:54:24 +0000

    plik z java

---
+++ sample.java
@@ -0,0 +1,11 @@
+// class sample {
+	// public static void main(String[] args){
+	// System.out.println("hello world");
+	// }
+// }
+
+class helloworldapp {
+    public static void main(string[] args) {
+        system.out.println("hello world!"); // display the string.
+    }
+}
